3a6c94a82bdb4b2551b62a1469a76a6428609957
loratokareva <loratokareva@gmail.com>
2017-06-08 17:33:07 +0300

    added /template edit /css and /img edit index.html and index-restaurant.html

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Навигация Гранд | Главная</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="/grandTitul/css/main.css">
+</head>
+<body>
+
+<main class="main-page">
+    <div class="container">
+        <section class="departments">
+
+           <?
+           include "template/form-search.html";
+           ?>
+
+
+            <?
+           include "template/first-page.html";
+
+
+
+           ?>
+
+        </section>
+    </div>
+</main>
+
+</body>
+</html>
+
---
+++ template/restaurant.php
@@ -0,0 +1,104 @@
+<!-- центральный блок -->
+<ul class="departments__list">
+
+    <li class="departments__item departments__item_carousel">
+
+       <?
+        include "template/carousel.html";
+       ?>
+
+    </li>
+    <!-- левый столбец кнопок -->
+    <li class="departments__item departments__item_invested">
+        <!-- верхний  мелкий -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-brown" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-fountain"></span>
+                                </span>
+                    <span class="btn__text">Кафе "У фонтана"</span>
+                </button>
+                <!-- /  -->
+            </li>
+        </ul>
+        <!-- /верхний  мелкий -->
+        <!-- средний  мелкий -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-brown" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-estetica"></span>
+                                </span>
+                    <span class="btn__text">Кафе "Estetica"</span>
+                </button>
+                <!-- /  -->
+            </li>
+        </ul>
+        <!-- /средний  мелкий -->
+        <!-- нижний мелкий  -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-gray" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-unico"></span>
+                                </span>
+                    <span class="btn__text">Кафе "Unico"</span>
+                </button>
+                <!-- /  -->
+            </li>
+        </ul>
+        <!-- /нижний мелкий  -->
+    </li>
+    <!-- /левый столбец кнопок -->
+    <!-- правый столбец кнопок -->
+    <li class="departments__item  departments__item_invested">
+        <!-- верхний  мелкий -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-brown" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-kivayaki"></span>
+                                </span>
+                    <span class="btn__text">Кафе "Киваяки"</span>
+                </button>
+                <!--  / -->
+            </li>
+        </ul>
+        <!-- /верхний  мелкий -->
+        <!-- средний  мелкий -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-brown" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-intepior"></span>
+                                </span>
+                    <span class="btn__text">Кафе "Intepior"</span>
+                </button>
+                <!-- /  -->
+            </li>
+        </ul>
+        <!-- /средний  мелкий -->
+        <!-- нижний мелкий  -->
+        <ul class="departments__list">
+            <li class="departments__item departments__item_restaurant">
+                <!--   -->
+                <button class="btn btn_theme btn_theme-lightgray" data-link="url-page">
+                                <span class="btn__background">
+                                    <span class="btn__pic btn__pic_cafe-vostochnoe"></span>
+                                </span>
+                    <span class="btn__text">Кафе "Восточное"</span>
+                </button>
+                <!--  / -->
+            </li>
+        </ul>
+        <!-- /нижний мелкий  -->
+    </li>
+    <!-- /правый столбец кнопок -->
+</ul>
+<!-- /центральный блок -->
